dbc95b06908851213e7dcda802c4a2f7093f9590
Luthfisyifa12 <luthfismarhas@gmail.com>
2025-02-25 13:24:24 +0700

    edit dan update

---
+++ inventory_luthfi/view/barang/proses_edit.php
@@ -0,0 +1,20 @@
+<?php
+$id_barang=$_GET['id_barang'];
+$id_jenis=$_POST['id_jenis'];
+$nama_barang=$_POST['nama_barang'];
+$harga=$_POST['harga'];
+$stok=$_POST['stok'];
+
+include '../../config/koneksi.php';
+
+$query=mysqli_query($conn, "UPDATE barang SET id_jenis='$id_jenis', nama_barang = '$nama_barang', harga = '$harga',
+stok = '$stok' WHERE id_barang='$id_barang'");
+
+if($query){
+    echo "<script>alert('Data berhasil diedit')</script>";
+    echo "<script>window.location.href='index.php'</script>";
+}else{
+    echo "<script>alert('Data gagal diedit')</script>";
+    echo "<script>window.location.href='view_tambah.php'</script>";
+}
+?>
